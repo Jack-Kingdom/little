fb7834e68031c4affbd3a2c78a292e333c9a0b25
jack <email@qiaohong.org>
2017-07-31 14:37:54 +0800

    modified backend graphql url

--- utils/updateSchema.js
+++ utils/updateSchema.js
@@ -4,8 +4,7 @@ import fetchSchema from 'fetch-graphql-schema'
 import config from '../config'
 
 const schemaPath = path.resolve(__dirname, '../graphql.schema.json');
-fetchSchema(config.backend_url + '/graphql')
-    .then(schemaJSON => fs.writeFileSync(schemaPath, schemaJSON));
+fetchSchema(config.backend_url + '/api/graphql').then(schemaJSON => fs.writeFileSync(schemaPath, schemaJSON));
 
 console.log('Wrote ' + schemaPath);
 
