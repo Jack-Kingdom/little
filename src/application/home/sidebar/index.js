3b2f7495ce23e106a83297633405123158762814
Jack King <email@qiaohong.org>
2017-08-07 08:17:18 +0800

    save

--- src/application/home/sidebar/index.js
+++ src/application/home/sidebar/index.js
@@ -25,7 +25,6 @@ class Sidebar extends React.Component {
                     <h4 className="sidebar-profile-name">Jack King</h4>
                     <h5 className="sidebar-profile-bio">Software developer</h5>
                 </div>
-
                 <ul className="sidebar-buttons">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/categories">Categories</Link></li>
@@ -35,20 +34,8 @@ class Sidebar extends React.Component {
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/admin">Admin</Link></li>
                 </ul>
-
-                <ul className="sidebar-buttons">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/categories">Categories</Link></li>
-                    <li><Link to="/tags">Tags</Link></li>
-                    <li><Link to="/archives">Archives</Link></li>
-                    <li><Link to="/search">Search</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/admin">Admin</Link></li>
-                </ul>
-
             </div>
         )
-            ;
     }
 }
 
