adb1e958135f09cc4614b8089b01d22aa7b7714b
Jack King <email@qiaohong.org>
2017-11-14 17:09:21 +0800

    code format

--- utils/webpackConfig.js
+++ utils/webpackConfig.js
@@ -1,69 +1,69 @@
-import path from 'path'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 let webpackConfig = {
-  entry: {
-    home: ['./src/home/index.js'],
-    admin: ['./src/admin/index.js']
-  },
-  output: {
-    path: path.join(__dirname, '../dist'),
-    filename: 'js/[name].js',
-    publicPath: '/'
-  },
-  devServer: {
-    port: 8080,
-    hot: true,
-    inline: true,
-    stats: {colors: true},
-    historyApiFallback: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: path.join(__dirname, '../node_modules'),
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'}
+    entry: {
+        home: ['./src/home/index.js'],
+        admin: ['./src/admin/index.js']
+    },
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'js/[name].js',
+        publicPath: '/'
+    },
+    devServer: {
+        port: 8080,
+        hot: true,
+        inline: true,
+        stats: {colors: true},
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: path.join(__dirname, '../node_modules'),
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'}
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url-loader?limit=8192&name=img/[hash:8].[ext]'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|svg)$/,
+                exclude: /node_modules/,
+                loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
+            }
         ]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader?limit=8192&name=img/[hash:8].[ext]'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        exclude: /node_modules/,
-        loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
-      }
+    },
+    plugins: [
+        // inject build file to html
+        new HtmlWebpackPlugin({
+            template: 'src/home/index.html',
+            chunks: ['home'],
+            filename: 'home.html',
+            minify: {
+                // todo minify html
+                removeComments: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/admin/index.html',
+            chunks: ['admin'],
+            filename: 'admin.html',
+            minify: {
+                // todo minify html
+                removeComments: true
+            }
+        })
     ]
-  },
-  plugins: [
-    // inject build file to html
-    new HtmlWebpackPlugin({
-      template: 'src/home/index.html',
-      chunks: ['home'],
-      filename: 'home.html',
-      minify: {
-        // todo minify html
-        removeComments: true
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/admin/index.html',
-      chunks: ['admin'],
-      filename: 'admin.html',
-      minify: {
-        // todo minify html
-        removeComments: true
-      }
-    })
-  ]
-}
+};
 
 export default webpackConfig
--- utils/webpackDev.js
+++ utils/webpackDev.js
@@ -1,31 +1,31 @@
-import webpack from 'webpack'
-import WebpackDevServer from 'webpack-dev-server'
-import webpackConfig from './webpackConfig'
-import config from '../config'
+import webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import webpackConfig from './webpackConfig';
+import config from '../config';
 
 // add hot reload support
-webpackConfig.entry.home.unshift(`webpack-dev-server/client?http://localhost:${webpackConfig.devServer.port}`)
-webpackConfig.entry.admin.unshift(`webpack-dev-server/client?http://localhost:${webpackConfig.devServer.port}`)
+webpackConfig.entry.home.unshift(`webpack-dev-server/client?http://localhost:${webpackConfig.devServer.port}`);
+webpackConfig.entry.admin.unshift(`webpack-dev-server/client?http://localhost:${webpackConfig.devServer.port}`);
 // add source map support
-webpackConfig.devtool = 'inline-source-map'
+webpackConfig.devtool = 'inline-source-map';
 
 const options = {
-  proxy: {
-    '/api/*': config.backend_url
-  },
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000
-  },
-  historyApiFallback: {
-    rewrites: [
-      {from: /^\/$/, to: '/home.html'},
-      {from: /^\/admin$/, to: '/admin.html'}
-    ]
-  }
-}
+    proxy: {
+        "/api/*": "http://localhost:23570"
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    historyApiFallback: {
+        rewrites: [
+            {from: /^\/$/, to: '/home.html'},
+            {from: /^\/admin$/, to: '/admin.html'}
+        ]
+    }
+};
 
-const server = new WebpackDevServer(webpack(webpackConfig), options)
+const server = new WebpackDevServer(webpack(webpackConfig), options);
 server.listen(webpackConfig.devServer.port, 'localhost', (err) => {
-  if (err) throw err
-})
+    if (err) throw err
+});
